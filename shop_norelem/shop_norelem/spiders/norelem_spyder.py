fad089f2fb0b498a7256a80ec56d2703ecf11ca9
Sana451 <sana451@mail.ru>
2024-08-17 23:46:38 +0300

    Сперва вызывается parse, который ищет все ссылки на конкретные продукты. Parse ожидает пока появится запрос на принятие куки, с помощью selenium подтверждает куки, скролит страницу, пока не появятся ссылки, после чего сохраняет их. Parse_product_page парсит непосредственно страницу с продуктом.

--- shop_norelem/shop_norelem/spiders/norelem_spyder.py
+++ shop_norelem/shop_norelem/spiders/norelem_spyder.py
@@ -7,6 +7,8 @@ import scrapy
 from scrapy.shell import inspect_response
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -28,20 +30,41 @@ class QuotesSpider(scrapy.Spider):
             if re_pattern.match(url.text):
                 urls.append(url.text)
 
-        self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        self.log(len(urls))
-        # self.log(urls)
-        self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # self.log(len(urls))
+        # # self.log(urls)
+        # self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-        for url in urls[:5]:
+        for url in urls[:10]:
             yield scrapy.Request(
                 url=url, callback=self.parse, meta={"playwright": True}
             )
             # yield SeleniumRequest(url=url, callback=self.parse)
 
-    # def parse_family_links(self, response):
-    #     # product_links = response.css("div.product-table a[data-id]").getat
-    #     inspect_response(response, self)
+    def parse_product_page(self, response):
+        # inspect_response(response, self)
+
+        yield {
+            "Заголовок товара": response.css("h1.product-details__name::text").get(),
+            "Артикул (Order number)": response.css(
+                "div.product-details__product-number::text"
+            ).get(),
+            # "Модель": "",
+            "Цена": Decimal(
+                response.css("p.price::text").get().strip().strip("€").replace(",", "")
+            ),
+            # "Цена со скидкой": "",
+            "Наличие": response.css(
+                "span.product-stock-level__localization::text"
+            ).get(),
+            "Картинки": f"https://norelem.de/{response.css('div.product-details-spacing img')[0].attrib['src']} | "
+            f"https://norelem.de/{response.css('div.product-details-spacing img')[1].attrib['src']}",
+            # "PDF": "",
+            # "Краткое описание": "",
+            # "Полное описание": "",
+            # "Характеристики": "",
+            # "Категории": "",
+        }
 
     def parse(self, response):
         # inspect_response(response, self)
@@ -57,35 +80,29 @@ class QuotesSpider(scrapy.Spider):
                     )
                 )
             )
-            # button = driver.find_element(
-            #     By.CSS_SELECTOR,
-            #     "button#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll",
-            # )
-            button.click()
-            driver.close()
-        inspect_response(response, self)
 
-        yield {
-            # "Заголовок товара": "response.css('h1.product-details__name::text').get()",
-            "Заголовок товара": response.css(
-                "h1.product-family-details__heading::text"
-            ).get(),
-            "Артикул": "",
-            "Модель": "",
-            "Цена": Decimal(
-                response.css(
-                    "div.-is-family-page span.product-price__net-price span::text"
+            button.click()
+
+            for i in range(50):
+                ActionChains(driver).scroll_by_amount(0, i).perform()
+
+            WebDriverWait(driver, 40).until(
+                EC.presence_of_element_located(
+                    (
+                        By.CSS_SELECTOR,
+                        "div.product-table a[data-id]",
+                    )
                 )
-                .get()
-                .strip("€")
-                .replace(",", "")
-            ),
-            "Цена со скидкой": "",
-            "Наличие": "",
-            "Картинки": "",
-            "PDF": "",
-            "Краткое описание": "",
-            "Полное описание": "",
-            "Характеристики": "",
-            "Категории": "",
-        }
+            )
+            a_tags = driver.find_elements(
+                By.CSS_SELECTOR, "div.product-table a[data-id]"
+            )
+
+            # self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            # self.log(a_tags)
+            # self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+            for a in a_tags:
+                product_page = a.get_attribute("href")
+                driver.close()
+                yield scrapy.Request(product_page, callback=self.parse_product_page)
