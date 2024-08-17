c63e2af32e403f937242bf0b0f0cb8f2a2a3c528
Sana451 <sana451@mail.ru>
2024-08-17 18:15:01 +0300

    При попытке попасть на страницу конкретного продукта всплывает окно, типа: "Принять cookie". Для нажатия этой кнопки пытаюсь использовать selenium. Кнопка то нажимается, однако scrapy пока об этом ничего не знает.

--- shop_norelem/shop_norelem/middlewares.py
+++ shop_norelem/shop_norelem/middlewares.py
@@ -101,6 +101,3 @@ class ShopNorelemDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
-
-
-DOWNLOADER_MIDDLEWARES = {"scrapy_selenium.SeleniumMiddleware": 800}
--- shop_norelem/shop_norelem/spiders/norelem_spyder.py
+++ shop_norelem/shop_norelem/spiders/norelem_spyder.py
@@ -1,9 +1,16 @@
+import time
+from decimal import Decimal
 import re
 import requests
 
 import scrapy
 from scrapy.shell import inspect_response
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class QuotesSpider(scrapy.Spider):
@@ -23,29 +30,55 @@ class QuotesSpider(scrapy.Spider):
 
         self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         self.log(len(urls))
+        # self.log(urls)
         self.log("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-        for url in urls:
+        for url in urls[:5]:
             yield scrapy.Request(
                 url=url, callback=self.parse, meta={"playwright": True}
             )
             # yield SeleniumRequest(url=url, callback=self.parse)
 
+    # def parse_family_links(self, response):
+    #     # product_links = response.css("div.product-table a[data-id]").getat
+    #     inspect_response(response, self)
+
     def parse(self, response):
         # inspect_response(response, self)
 
+        if response.css("button#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"):
+            driver = webdriver.Chrome()
+            driver.get(response.url)
+            button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (
+                        By.CSS_SELECTOR,
+                        "button#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll",
+                    )
+                )
+            )
+            # button = driver.find_element(
+            #     By.CSS_SELECTOR,
+            #     "button#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll",
+            # )
+            button.click()
+            driver.close()
+        inspect_response(response, self)
+
         yield {
             # "Заголовок товара": "response.css('h1.product-details__name::text').get()",
-            "Заголовок товара": "response.css('h1.product-details__name::text').get()",
+            "Заголовок товара": response.css(
+                "h1.product-family-details__heading::text"
+            ).get(),
             "Артикул": "",
             "Модель": "",
-            "Цена": float(
+            "Цена": Decimal(
                 response.css(
                     "div.-is-family-page span.product-price__net-price span::text"
                 )
                 .get()
                 .strip("€")
-                .replace(",", ".")
+                .replace(",", "")
             ),
             "Цена со скидкой": "",
             "Наличие": "",
